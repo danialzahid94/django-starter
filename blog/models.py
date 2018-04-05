fcf0bcbf6b8f7db986b7f1fc2d3d4c16e2ee9495
Danial Zahid <danialzahid94@live.com>
2018-04-05 15:16:52 +0500

    Changed blog model according to demo

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+
 class Blog(models.Model):
     title = models.CharField(max_length=250)
+    pub_date = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to='images/')
     body = models.CharField(max_length=1000)
