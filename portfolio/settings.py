b89d14171bff1ada87e4096e21473c002060a645
Danial Zahid <danialzahid94@live.com>
2018-04-17 12:12:13 +0500

    Added bootstrap and made jobs work

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -79,10 +79,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
-        'USER':'postgres',
-        'PASSWORD':'django123',
-        'HOST':'localhost',
-        'PORT':'5432',
+        'USER': 'postgres',
+        'PASSWORD': 'django123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
